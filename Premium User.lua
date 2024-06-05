076f03e5311a5ae076d63760776093640c0f20a3
NittarPP <167613469+NittarPP@users.noreply.github.com>
2024-06-05 20:52:37 +0700

    Update Premium User.lua

--- Premium User.lua
+++ Premium User.lua
@@ -1,1 +1,12 @@
 local id = {"2308948481"}
+
+return {
+    find = function(playerId)
+        for _, premiumId in ipairs(id) do
+            if tostring(playerId) == premiumId then
+                return true
+            end
+        end
+        return false
+    end
+}
